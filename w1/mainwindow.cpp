69b51935e52927ffebfc64dada59e8b89b576f32
Andrew nuark G <mrcluster@ya.ru>
2020-11-06 11:17:29 +0700

    Add 'Authors' activation trigger handler

--- w1/mainwindow.cpp
+++ w1/mainwindow.cpp
@@ -1,15 +1,16 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
     ui->setupUi(this);
+
+    connect(ui->act_authors, &QAction::triggered, this, &MainWindow::About_Lab1);
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow() {
     delete ui;
 }
 
+void MainWindow::About_Lab1() {
+
+}
--- w1/mainwindow.h
+++ w1/mainwindow.h
@@ -7,15 +7,17 @@ QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
+class MainWindow : public QMainWindow {
+Q_OBJECT
+
+private:
+    Ui::MainWindow *ui;
 
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
-private:
-    Ui::MainWindow *ui;
+private slots:
+    void About_Lab1();
 };
 #endif // MAINWINDOW_H
