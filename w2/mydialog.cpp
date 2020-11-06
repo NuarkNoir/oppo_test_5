5c79cd8b7efeeef6f4ebf310d92363b1a6274e60
Andrew nuark G <mrcluster@ya.ru>
2020-11-06 14:20:17 +0700

    Add data save feature

--- w2/mydialog.cpp
+++ w2/mydialog.cpp
@@ -4,36 +4,58 @@
 MyDialog::MyDialog(QWidget *parent) : QDialog(parent) {
     setWindowTitle("Анкета");
 
-    QVBoxLayout *v_lay = new QVBoxLayout;
+    this->v_lay = new QVBoxLayout;
 
 
     QHBoxLayout *first_row = new QHBoxLayout;
-    QSpinBox *sb_spinner = new QSpinBox();
-    QTextEdit *te_text = new QTextEdit();
+    this->sb_spinner = new QSpinBox();
+    this->te_text = new QTextEdit();
 
     first_row->addWidget(sb_spinner);
     first_row->addWidget(te_text);
 
-    v_lay->addLayout(first_row);
+    this->v_lay->addLayout(first_row);
 
 
     QHBoxLayout *second_row = new QHBoxLayout;
-    QComboBox *cb_variants = new QComboBox();
-    QPushButton *btn_save = new QPushButton();
+    this->cb_variants = new QComboBox();
+    this->btn_save = new QPushButton();
 
     second_row->addWidget(cb_variants);
     second_row->addWidget(btn_save);
 
-    v_lay->addLayout(second_row);
+    this->v_lay->addLayout(second_row);
 
 
     this->setLayout(v_lay);
 
     cb_variants->addItems({"First", "Second", "Third"});
     btn_save->setText("Save data");
+
+    connect(btn_save, &QPushButton::clicked, this, &MyDialog::OnSaveCalled);
 }
 
 MyDialog::~MyDialog() {
+    delete this->v_lay;
 
+    delete this->sb_spinner;
+    delete this->te_text;
+    delete this->cb_variants;
+
+    delete this->btn_save;
 }
 
+void MyDialog::OnSaveCalled() {
+    QString file_path = "./output_data.text";
+
+    QFile file(file_path);
+    file.open(QIODevice::ReadWrite | QIODevice::Truncate | QIODevice::Text);
+
+    QTextStream in(&file);
+    in << "Spinner value: " << this->sb_spinner->value() << Qt::endl;
+    in << "TextEdit value: " << this->te_text->toPlainText() << Qt::endl;
+    in << "Spinner value: " << this->cb_variants->currentText() << Qt::endl;
+
+    in.flush();
+    file.close();
+}
--- w2/mydialog.h
+++ w2/mydialog.h
@@ -8,14 +8,26 @@
 #include <QTextEdit>
 #include <QComboBox>
 #include <QPushButton>
+#include <QFile>
+#include <QIODevice>
+#include <QTextStream>
 
 class MyDialog : public QDialog {
     Q_OBJECT
 
 private:
+    QVBoxLayout *v_lay;
+
+    QSpinBox *sb_spinner;
+    QTextEdit *te_text;
+    QComboBox *cb_variants;
+    QPushButton *btn_save;
 
 public:
-    MyDialog(QWidget *parent = nullptr);
+    MyDialog(QWidget *parent=nullptr);
     ~MyDialog();
+
+private slots:
+    void OnSaveCalled();
 };
 #endif // MYDIALOG_H
