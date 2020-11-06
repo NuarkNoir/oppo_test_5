49e355bc83f30176fdb455480bb87e8d2ecb963a
Andrew nuark G <mrcluster@ya.ru>
2020-11-06 12:11:47 +0700

    Add text style change for first day of week

--- w1/mainwindow.cpp
+++ w1/mainwindow.cpp
@@ -17,5 +17,15 @@ void MainWindow::About_Lab1() {
 }
 
 void MainWindow::ChangedComboxIndex(int newIdx) {
-    ui->cal_curr_year->setFirstDayOfWeek(Qt::DayOfWeek(newIdx));
+    ui->cal_curr_year->setFirstDayOfWeek(Qt::DayOfWeek(newIdx+1));
+
+    for (int i = 1; i < 8; i++) {
+        QTextCharFormat format;
+
+        if (i == newIdx+1) {
+            format.setForeground(qvariant_cast<QColor>("green"));
+        }
+
+        ui->cal_curr_year->setWeekdayTextFormat(Qt::DayOfWeek(i), format);
+    }
 }
